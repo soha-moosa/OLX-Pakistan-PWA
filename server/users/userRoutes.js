6a13253f833c280d8b34918d2c2e9cfe0e5705cd
soha-moosa <sohamoosa153@gmail.com>
2018-08-20 17:40:16 +0500

    User Routes

---
+++ server/users/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('./userController');
+
+// router.post('/signUp', controller.signUp);
+// router.post('/login', controller.login);
