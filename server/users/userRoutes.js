dbdea05b0f30f5465ebc6f16666354bb4e8a6839
soha-moosa <sohamoosa153@gmail.com>
2018-08-26 00:52:25 +0500

    User routes file

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const mongoose = require('mongoose');
 const controller = require('./userController');
 
-// router.post('/signUp', controller.signUp);
-// router.post('/login', controller.login);
+router.post('/signup', controller.signup);
+router.post('/login', controller.login);
+router.delete('/:id', controller.deleteUser);
+
+
+module.exports = router;
