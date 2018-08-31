5c465fe063c940e7160c6b362ae8e98b883c1847
soha-moosa <sohamoosa153@gmail.com>
2018-08-31 16:50:23 +0500

    Product Routes

--- server/products/productRoutes.js
+++ server/products/productRoutes.js
@@ -2,41 +2,42 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const controller = require('./productController');
-const checkAuth = require('../middleware/check-auth');
+// const checkAuth = require('../middleware/check-auth');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads/');
-    },
-    filename: (req, file, cb) => {
-        // cb(null, new Date().toISOString() + file.originalname);
-        var fileFormat = (file.originalname).split(".");
-        cb(null, file.fieldname + '-' + Date.now() + '.' + fileFormat[fileFormat.length - 1]);
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, './uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//         // cb(null, new Date().toISOString() + file.originalname);
+//         var fileFormat = (file.originalname).split(".");
+//         cb(null, file.fieldname + '-' + Date.now() + '.' + fileFormat[fileFormat.length - 1]);
 
-    }
-});
-const fileFilter = (req, file, cb) => {
-    //reject a file
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-});
+//     }
+// });
+// const fileFilter = (req, file, cb) => {
+//     //reject a file
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//         cb(null, true);
+//     } else {
+//         cb(null, false);
+//     }
+// };
+// const upload = multer({
+//     storage: storage,
+//     limits: {
+//         fileSize: 1024 * 1024 * 5
+//     },
+//     fileFilter: fileFilter
+// });
 
+// upload.single('productImage'),
 
-router.post('/addProduct', checkAuth, upload.single('productImage'), controller.addProduct);
+router.post('/addProduct',  controller.addProduct);
 router.get('/allProduct', controller.getAllProduct);
 router.get('/searchById/:id', controller.searchProduct);
-router.post('/searchByCategory', checkAuth, controller.searchByCategory);
-router.post('/searchByName', checkAuth, controller.searchByName);
-router.delete('/deleteProduct/:id', checkAuth, controller.deleteProduct);
+router.post('/searchByCategory', controller.searchByCategory);
+router.post('/searchByName', controller.searchByName);
+router.delete('/deleteProduct/:id', controller.deleteProduct);
 module.exports = router;
 
